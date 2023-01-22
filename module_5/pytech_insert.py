faf2036f7c4618df6ed1908625df5e0e428150b3
Sapphire-Rose <102334226+Sapphire-Rose@users.noreply.github.com>
2023-01-21 19:46:17 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,24 +1,106 @@
+#import
 from pymongo import MongoClient
+
+#Mongo connection string
 url = "mongodb+srv://admin:admin@cluster0.cgua3dr.mongodb.net/pytech?retryWrites=true&w=majority"
+
+#Mongo Connection cluster
 client = MongoClient(url)
+
+#pytech database
 db = client.pytech
+
+#student documents
 rand = {
     "student_id": "1007",
     "first_name": "Rand",
-    "last_name": "al'Thor"
+    "last_name": "al'Thor",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "4.0",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Swords and Sword Accessories",
+                    "instructor": "Professor Merrilin",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "The Art of Magic",
+                    "instructor": "Professor Merrilin",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
 }
+
 mat = {
     "student_id": "1008",
     "first_name": "Mat",
-    "last_name": "Cauthon"
+    "last_name": "Cauthon",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "4.0",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Swords and Sword Accessories",
+                    "instructor": "Professor Merrilin",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "The Art of Magic",
+                    "instructor": "Professor Merrilin",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
 }
+
 perrin = {
     "student_id": "1009",
     "first_name": "Perrin",
-    "last_name": "Aybara"
+    "last_name": "Aybara",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "4.0",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Swords and Sword Accessories",
+                    "instructor": "Professor Merrilin",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "The Art of Magic",
+                    "instructor": "Professor Merrilin",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
 }
+
+#student collection
 students = db.students
+
+#insert statements
 print("\n  Inserting... ")
+
 rand_student_id = students.insert_one(rand).inserted_id
 print("  Inserted student record for Rand al'Thor into the students collection with document_id " + str(rand_student_id))
 
